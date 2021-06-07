60c7a02154c7b0a9db03e63d68a31408e668d34a
kfirle <49530231+kfirle@users.noreply.github.com>
2021-06-08 01:13:51 +0300

    part B

--- submission.py
+++ submission.py
@@ -2,7 +2,7 @@ import logic
 import random
 import math
 import numpy as np
-from copy import copy
+from copy import deepcopy
 from AbstractPlayers import *
 import time
 
@@ -14,8 +14,10 @@ import time
 commands = {Move.UP: logic.up, Move.DOWN: logic.down,
             Move.LEFT: logic.left, Move.RIGHT: logic.right}
 
-### global functions and consts
+# global functions and consts
+
 victory_value = 2048
+time_margin = 0.05
 
 
 def empty_cells_calc(board) -> int:
@@ -25,6 +27,17 @@ def empty_cells_calc(board) -> int:
             if board[i][j] == 0:
                 empty_cells += 1
     return empty_cells
+
+
+def board_score_calc(board) -> int:
+    score = 0
+    for i in range(0, len(board)):
+        for j in range(0, len(board)):
+            current = board[i][j]
+            while current > 2:
+                score += current
+                current = current / 2
+    return score
 
 
 def evaluation_function(board, score):
@@ -51,6 +64,113 @@ def is_final_state(board) -> bool:
             if board[i][j] > max_val:
                 max_val = board[i][j]
     return (empty_cells == 0) or (max_val == victory_value)
+
+
+def return_max(successors_values):
+    """
+    :param successors_values: list of tuple: (successor, value)
+    :return: tuple with maximal value
+    """
+    if len(successors_values) == 0:
+        return -np.inf
+    return max(successors_values)
+
+
+def return_min(successors_values):
+    """
+    :param successors_values: list of tuple: (successor, value)
+    :return: tuple with maximal value
+    """
+    if len(successors_values) == 0:
+        return np.inf
+    return min(successors_values)
+
+
+# functions for the second heuristic function
+
+def gradient(board):
+    """
+    Calculates how much the grid is uniforaml - if all the cells
+    are equal, the function will return 0. Otherwise, it will return
+    the inverse of sum of all differences (in absolute value) between neighbours in the grid.
+    """
+    simpleGrid = [[0 for i in range(4)] for j in range(4)]
+    for row in range(0, 4):
+        for col in range(0, 4):
+            simpleGrid[row][col] = board[row][col]
+
+    bonus = 0
+    for row in range(4):
+        for col in range(4):
+            if simpleGrid[row][col] != 0:
+                if simpleGrid[row][3] != 0:
+                    bonus += abs(math.log2(simpleGrid[row][col]) - math.log2(
+                        simpleGrid[row][3]))
+                if simpleGrid[3][col] != 0:
+                    bonus += abs(math.log2(simpleGrid[row][col]) - math.log2(
+                        simpleGrid[3][col]))
+
+    return -bonus
+
+
+def direction(board):
+    """ Calculates how much the grid is organized in some direction. """
+    simpleGrid = [[0 for i in range(4)] for j in range(4)]
+    for row in range(0,4):
+        for col in range(0,4):
+            if(board[row][col] != 0):
+                simpleGrid[row][col] = math.log2(board[row][col])
+            else:
+                simpleGrid[row][col] = 0
+
+    asndud = 0
+    dsndud = 0
+    asndlr = 0
+    dsndlr = 0
+    for row in range(4):
+        for col in range(4):
+            if col + 1 < 4:
+                if simpleGrid[row][col] > simpleGrid[row][col + 1]:
+                    dsndlr -= simpleGrid[row][col] - simpleGrid[row][col + 1]
+                else:
+                    asndlr += simpleGrid[row][col] - simpleGrid[row][col + 1]
+            if row + 1 < 4:
+                if simpleGrid[row][col] > simpleGrid[row + 1][col]:
+                    dsndud -= simpleGrid[row][col] - simpleGrid[row + 1][col]
+                else:
+                    asndud += simpleGrid[row][col] - simpleGrid[row + 1][col]
+    return max(dsndlr, asndlr) + max(dsndud, asndud)
+
+
+def highestTile(board):
+    """ Returns the maximal tile. """
+    maximal =  max(max(x) for x in board)
+    return maximal
+
+
+def score(board):
+    """ Calculates the inverse sum of all the log values of the tiles. """
+    sum = 0
+    for row in range(0, 4):
+        for col in range(0, 4):
+            if board[row][col] != 0:
+                sum -= math.log2(board[row][col])
+    return sum
+
+
+def moveGrader(board):
+    emptFact = 25
+    highestFact = 10
+    scoregrade = 14
+    gradientFac = 1
+    directionFac = 15
+    highest = highestTile(board)
+    empty = empty_cells_calc(board)
+    if empty != 0:
+        empty = math.log(empty)
+    val = emptFact * empty + highestFact * highest + score(board) * scoregrade + direction(board) * directionFac\
+          + gradient(board) * gradientFac
+    return val
 
 
 # generate value between {2,4} with probability p for 4
@@ -106,38 +226,12 @@ class ImprovedGreedyMovePlayer(AbstractMovePlayer):
             if done:
                 optional_moves_score[move] = (score - self.previous_score) + math.log2(score + 2) * (empty_cells_calc(new_board))\
                                              + 2 * new_board [0][0] + 0.5 * new_board [0][1] + 0.25 * new_board [0][2]
-                #optional_moves_score[move] = self.moveGrader(new_board)
                 if score > next_score:
                     next_score = score
         self.previous_score = next_score
         return max(optional_moves_score, key=optional_moves_score.get)
 
     # TODO: add here helper functions in class, if needed
-
-    """ Returns the maximal tile. """
-
-    def highestTile(self, board):
-        maximal =  max(max(x) for x in board)
-        return maximal
-
-    """ Calculates the inverse sum of all the log values of the tiles. """
-
-    def score(self, board):
-        sum = 0
-        for row in range(0, 4):
-            for col in range(0, 4):
-                if board[row][col] != 0:
-                    sum -= math.log2(board[row][col])
-        return sum
-
-    def moveGrader(self, board):
-        emptFact = 25
-        highestFact = 10
-        scoregrade = 14
-        highest = self.highestTile(board)
-        empty = empty_cells_calc(board)
-        val = emptFact * empty + highestFact * highest + self.score(board) * scoregrade
-        return val
 
 
 # part B
@@ -149,18 +243,36 @@ class MiniMaxMovePlayer(AbstractMovePlayer):
     def __init__(self):
         AbstractMovePlayer.__init__(self)
         # TODO: add here if needed
+        self.iteration_start_time = None
+        self.iteration_time = 1
+        self.timeout_flag = False
 
     def get_move(self, board, time_limit) -> Move:
         # TODO: erase the following line and implement this function.
-        optional_moves_score = {}
-        depth = 1
-        for move in Move:
-            new_board, done, score = commands[move](board)
-            if done:
-                move_value = self.rec_minmax(depth - 0.5, new_board, evaluation_function(new_board, score))
-                if move_value >= -np.inf:
-                    optional_moves_score[move] = move_value
-        return max(optional_moves_score, key=optional_moves_score.get)
+        temp_max = Move.UP
+        depth = 0
+        self.iteration_start_time = time.time()
+        self.iteration_time = time_limit
+        self.timeout_flag = False
+        while time.time() - self.iteration_start_time < time_limit - time_margin:
+            optional_moves_score = {}
+            depth += 1
+            for move in Move:
+                new_board, done, score = commands[move](board)
+                if done:
+                    #move_value = self.rec_minmax(depth - 0.5, new_board, evaluation_function(new_board, score))
+                    if time.time() - self.iteration_start_time < time_limit - time_margin:
+                        local_start = time.time()
+                        move_value = self.rec_minmax(depth - 0.5, new_board, moveGrader(new_board))
+                        #print("Move time: " + str(time.time()-local_start) + " depth: " + str(depth))
+                        if move_value >= -np.inf:
+                            optional_moves_score[move] = move_value
+                    else:
+                        return temp_max
+            if self.timeout_flag is False:
+                temp_max = max(optional_moves_score, key=optional_moves_score.get)
+        return temp_max
+
 
     # TODO: add here helper functions in class, if needed
     def rec_minmax(self, depth, board, eval_value=0):
@@ -171,7 +283,12 @@ class MiniMaxMovePlayer(AbstractMovePlayer):
         :param player: 0 - player, 1 - opponent
         :return: value of the state according to minimax
         '''
-        if depth == 0 or is_final_state(board):
+        # if no time left
+        if time.time() - self.iteration_start_time >= self.iteration_time - time_margin:
+            self.timeout_flag = True
+            return eval_value
+
+        if depth == 0:  #or is_final_state(board):
             return eval_value
         # maximize
         if round(depth) == depth:  # depth = round number
@@ -179,52 +296,22 @@ class MiniMaxMovePlayer(AbstractMovePlayer):
             for move in Move:
                 new_board, done, score = commands[move](board)
                 if done:
-                    optional_moves_score[move] = new_board, evaluation_function(new_board,score)
-            return self.return_max([self.rec_minmax(depth - 0.5, new_board, new_score) \
-                                    for new_board, new_score in optional_moves_score])
+                    #optional_moves_score[move] = new_board, evaluation_function(new_board,score)
+                    optional_moves_score[move] = new_board, moveGrader(new_board)
+            return return_max([self.rec_minmax(depth - 0.5, optional_moves_score[move1][0], optional_moves_score[move1][1])\
+                               for move1 in optional_moves_score])
         # minimize
         else:
             index_next_boards = []
             for tup in get_empty_indices(board):
-                new_board = copy(board)
+                new_board = deepcopy(board)
                 i = int(tup[0])
                 j = int(tup[1])
                 new_board[i][j] = 2
                 index_next_boards.append(new_board)
-            return self.return_min([self.rec_minmax(depth - 0.5, new_board, eval_value) \
+            return return_min([self.rec_minmax(depth - 0.5, new_board, eval_value) \
                                     for new_board in index_next_boards])
 
-    def return_max(self, successors_values):
-        '''
-        :param successors_values: list of tuple: (successor, value)
-        :return: tuple with maximal value
-        '''
-        if len(successors_values) == 0:
-            return -np.inf
-        return max(successors_values)
-
-    def return_min(self, successors_values):
-        '''
-        :param successors_values: list of tuple: (successor, value)
-        :return: tuple with maximal value
-        '''
-        if len(successors_values) == 0:
-            return np.inf
-        return min(successors_values)
-
-    def get_action(self, board):
-        """
-        Returns the minimax action from the current gameState using self.depth
-        and self.evaluationFunction.
-        Here are some method calls that might be useful when implementing minimax.
-        game_state.get_legal_actions(agent_index):
-            Returns a list of legal actions for an agent
-            agent_index=0 means our agent, the opponent is agent_index=1
-        Action.STOP:
-            The stop direction, which is always legal
-        game_state.generate_successor(agent_index, action):
-            Returns the successor game state after an agent takes an action
-        """
 
 
 class MiniMaxIndexPlayer(AbstractIndexPlayer):
@@ -237,45 +324,77 @@ class MiniMaxIndexPlayer(AbstractIndexPlayer):
     def __init__(self):
         AbstractIndexPlayer.__init__(self)
         # TODO: add here if needed
+        self.iteration_start_time = None
+        self.iteration_time = 1
+        self.timeout_flag = False
 
     def get_indices(self, board, value, time_limit) -> (int, int):
-        empty_cells = get_empty_indices(board)
-        optional_moves_score = {}
-        for move in empty_cells:
-            new_board, done, score = commands[move](board)
-            if done:
-                optional_moves_score[move] = score
-        return min(optional_moves_score, key=optional_moves_score.get)
+        depth = 0
+        empty = get_empty_indices(board)
+        temp_min = empty[0]
+        self.iteration_start_time = time.time()
+        self.iteration_time = time_limit
+        self.timeout_flag = False
+        while time.time() - self.iteration_start_time < time_limit - time_margin:
+            optional_index_score = {}
+            depth += 1
+            # score = board_score_calc(board)
+            for tup in empty:
+                new_board = deepcopy(board)
+                i = int(tup[0])
+                j = int(tup[1])
+                new_board[i][j] = 2
+                #move_value = self.rec_minmax(depth - 0.5, new_board, evaluation_function(new_board, score))
+                if time.time() - self.iteration_start_time < time_limit - time_margin:
+                    local_start = time.time()
+                    move_value = self.rec_minmax(depth - 0.5, new_board, moveGrader(new_board))
+                    #print("Index time: " + str(time.time() - local_start) + " depth: " + str(depth))
+                    if move_value <= np.inf:
+                        optional_index_score[(i,j)] = move_value
+                else:
+                    return temp_min
+            if self.timeout_flag is False:
+                temp_min = min(optional_index_score, key= optional_index_score.get)
+        return temp_min
 
     # TODO: add here helper functions in class, if needed
     def rec_minmax(self, depth, board, eval_value=0):
-        '''
+        """
         a recursive algorithm to calculate minimax algorithm
         :param depth: tracking our depth in the tree
         :param game_state: A stateof the game
         :param player: 0 - player, 1 - opponent
         :return: value of the state according to minimax
-        '''
-        if depth == 0 or is_final_state(board):
+        """
+        # if no time left
+
+        if time.time() - self.iteration_start_time >= self.iteration_time - time_margin:
+            self.timeout_flag = True
             return eval_value
+        if depth == 0: # or is_final_state(board):
+            return eval_value
+        # minimize
+        if round(depth) == depth:  # depth = round number
+            index_next_boards = []
+            for tup in get_empty_indices(board):
+                new_board = deepcopy(board)
+                i = int(tup[0])
+                j = int(tup[1])
+                new_board[i][j] = 2
+                index_next_boards.append(new_board)
+            return return_min([self.rec_minmax(depth - 0.5, new_board, eval_value)\
+                               for new_board in index_next_boards])
         # maximize
-        if round(depth) != depth:  # depth = round number
+        else:
             optional_moves_score = {}
             for move in Move:
                 new_board, done, score = commands[move](board)
                 if done:
-                    optional_moves_score[move] = new_board, evaluation_function(new_board,score)
-            return self.return_max([self.rec_minmax(depth - 0.5, new_board, new_score) \
-                                    for new_board, new_score in optional_moves_score])
-        # minimize
-        else:
-            index_next_boards = {}
-            for i, j in get_empty_indices(board):
-                new_board = copy(board)
-                new_board[i][j] = 2
-                index_next_boards[i][j] = new_board
-            return self.return_min([self.rec_minmax(depth - 0.5, new_board, eval_value) \
-                                    for new_board in index_next_boards])
+                    #optional_moves_score[move] = new_board, evaluation_function(new_board, score)
+                    optional_moves_score[move] = new_board, moveGrader(new_board)
+            return return_max([self.rec_minmax(depth - 0.5, optional_moves_score[move1][0], optional_moves_score[move1][1])\
+                               for move1 in optional_moves_score])
+
 
 # part C
 class ABMovePlayer(AbstractMovePlayer):
